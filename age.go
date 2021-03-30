0be8b84fe616dea8f180d7ed0d8d938be9be302a
SphericalKat <amolele@gmail.com>
2021-03-30 23:55:59 +0530

    feat: expose encryption and decryption functions
    Signed-off-by: SphericalKat <amolele@gmail.com>

---
+++ age.go
@@ -0,0 +1,51 @@
+package age_mobile
+
+import (
+	"bytes"
+	"io"
+
+	"filippo.io/age"
+)
+
+func Encrypt(in []byte, publicKey string) ([]byte, error) {
+	recipient, err := age.ParseX25519Recipient(publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	out := &bytes.Buffer{}
+	w, err := age.Encrypt(out, recipient)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := w.Write(in); err != nil {
+		return nil, err
+	}
+
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+
+	return out.Bytes(), nil
+}
+
+func Decrypt(in []byte, privateKey string) ([]byte, error) {
+	identity, err := age.ParseX25519Identity(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	out := &bytes.Buffer{}
+	
+	r, err := age.Decrypt(bytes.NewBuffer(in), identity)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := io.Copy(out, r); err != nil {
+		return nil, err
+	}
+
+	return out.Bytes(), nil
+}
